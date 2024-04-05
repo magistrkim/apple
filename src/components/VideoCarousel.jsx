aa627b294174f4a1bbf9ae3445befb48254720c0
Nataliya Kim <magistr1982@gmail.com>
2024-04-05 13:55:29 +0300

    Add video to carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,7 +1,28 @@
-
+import { hightlightsSlides } from "../constants";
 
 export const VideoCarousel = () => {
   return (
-    <div>VideoCarousel</div>
-  )
-}
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((item) => (
+          <div key={item.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={item.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {item.textLists.map((text) => (
+                  <p key={text} className="text-xl md:text-2xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
