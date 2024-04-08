77bdb03a0d47c12adfc4d441bd95c75e95904cf4
Nataliya Kim <magistr1982@gmail.com>
2024-04-08 13:42:22 +0300

    Create Lights component

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -90,8 +90,20 @@ const Model = () => {
                   />
                 ))}
               </ul>
-                          <button className="size-btn-container">
-                              
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
               </button>
             </div>
           </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,26 @@
+import { PerspectiveCamera, View } from "@react-three/drei";
 
-
-const ModelView = () => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
   return (
-    <div>ModelView</div>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={`border - 2 border-red-700 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+      >
+          <ambientLight intensity={0.3} />
+          <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+    </View>
+  );
+};
 
-export default ModelView
+export default ModelView;
