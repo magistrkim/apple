36b149bbea7a147328b4e613e2e3115d9ab9b01f
Nataliya Kim <magistr1982@gmail.com>
2024-04-10 07:25:27 +0300

    Fix the video animation

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,11 +2,21 @@ import { useGSAP } from "@gsap/react";
 import { animateWithGsap } from "../utils/animation";
 import { explore1Img, explore2Img, exploreVideo } from "../utils";
 import { useRef } from "react";
+import gsap from "gsap";
 
 const Features = () => {
   const videoRef = useRef();
   useGSAP(() => {
-    animateWithGsap("#exploreVideo");
+    gsap.to("#exploreVideo", {
+      scrollTrigger: {
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: "-10% bottom",
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      },
+    });
     animateWithGsap("#features_title", {
       y: 0,
       opacity: 1,
